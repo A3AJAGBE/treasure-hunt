e7c5aaa619dc00e3f409fc4293e853ea2842ca56
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-01-20 06:55:00 +0000

    Add start prompts and statement

--- main.py
+++ main.py
@@ -6,5 +6,15 @@ from logo import logo
 
 # Displays at the beginning
 print(logo)
-print("Let the HUNT begin\n")
+print("Welcome on this awesome adventure\n")
 
+start = input("Are you are ready to begin? Yes or No \n").title()
+
+# Verify the question response
+if start == "Yes":
+    print("Alright let the hunt begin!!!\n")
+    print("***********************************************\n")
+elif start == "No":
+    print("Restart the game, when you are ready")
+else:
+    print("Invalid Response")
